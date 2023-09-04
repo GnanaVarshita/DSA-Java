cbd95769fea7301a8d14d7e75c6431eedb3dc9eb
GnanaVarshita <gnanavarshitagv@gmail.com>
2023-09-04 21:35:25 +0530

    12-commit

--- QuickSort/src/Main.java
+++ QuickSort/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr={5,4,3,2,1};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] nums,int low, int hi){
+        if(low>=hi){
+            return;
+        }
+        int s = low;
+        int e=hi;
+        int m= s+(e-s)/2;
+        int pivot=nums[m];
+
+        while(s<=e){
+            while(nums[s]<pivot){
+                s++;
+            }
+            while(nums[e]>pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp = nums[s];
+                nums[s]= nums[e];
+                nums[e]= temp;
+                s++;
+                e--;
+            }
+        }
+        //now my pivot is at crct index, sort other halfes as well
+        sort(nums,low,e);
+        sort(nums,s, hi);
     }
 }
