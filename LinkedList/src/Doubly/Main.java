65c63f244ebdd4a61cbb3a6f554bbdefa38c3e3a
GnanaVarshita <gnanavarshitagv@gmail.com>
2023-09-18 16:36:51 +0530

    15-commit

---
+++ LinkedList/src/Doubly/Main.java
@@ -0,0 +1,14 @@
+package Doubly;
+
+public class Main {
+    public static void main(String[] args) {
+          Dl list = new Dl();
+          list.insertFirst(5);
+          list.insertFirst(2);
+          list.insertFirst(7);
+          list.insertFirst(9);
+          list.insertFirst(6);
+
+          list.display();
+    }
+}
---
+++ LinkedList/src/Singly/Main.java
@@ -0,0 +1,19 @@
+package Singly;
+
+import Singly.LL;
+
+public class Main {
+    public static void main(String[] args) {
+       LL list = new LL();
+       list.insertFirst(7);
+       list.insertFirst(2);
+        list.insertFirst(9);
+        list.insertFirst(8);
+        list.insertFirst(14);
+         list.insert(100,5);
+        list.insertLast(18);
+
+        list.display();
+
+    }
+}
