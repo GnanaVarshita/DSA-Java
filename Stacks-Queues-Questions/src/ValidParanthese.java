caa70d27a67da88043d242b84fc2d952ab946591
GnanaVarshita <gnanavarshitagv@gmail.com>
2023-09-24 10:05:48 +0530

    17-commit

---
+++ Stacks-Queues-Questions/src/ValidParanthese.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+public class ValidParanthese {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='{'||ch=='('||ch=='['){
+                stack.push(ch);
+            }
+
+            else{
+                if(ch=='}'){
+                    if(stack.pop()!='}'||stack.isEmpty()){
+                        return false;
+                    }
+                }
+                if(ch==')'){
+                    if(stack.pop()!=')'||stack.isEmpty()){
+                        return false;
+                    }
+                }
+
+                if(ch==']'){
+                    if(stack.pop()!=']'||stack.isEmpty()){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+}
